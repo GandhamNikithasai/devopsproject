756bd6e39ce5585fdffd72879154bd4edbee16d0
nikitha sai <nikithapatel@gmail.com>
2024-12-03 14:11:19 +0530

    addition.java with content nikitha sai

---
+++ addition.java
@@ -0,0 +1,13 @@
+public class Addition {
+    public static void main(String[] args) {
+        // Define two numbers
+        int num1 = 5;
+        int num2 = 3;
+
+        // Perform addition
+        int sum = num1 + num2;
+
+        // Output the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
